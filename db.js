8499ddc510502f618d9dabf20f001a2238f51260
QoltyQ <janathan186@gmail.com>
2022-09-20 01:30:23 +0600

    db

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const {Sequelize} = require('sequelize');
 const {NodeSSH} = require('node-ssh')
 const {DataTypes} = require('sequelize');
 
-const sequelize = new Sequelize('postgres://postgres:B6d3k9GA@127.0.0.1:5432/checkauto_bot');
+const sequelize = new Sequelize('postgres://postgres:B6d3k9GA@78.40.109.128:5432/checkauto_bot');
 // const sequelize = new Sequelize('postgres://check_auto:B6d3k9GA@127.0.0.1:5432/checkauto-bot');
 
 const testingConnection = async () => {
